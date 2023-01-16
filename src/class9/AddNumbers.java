324a12951b0b59d480761fba5939d29de32ae437
tirgani12 <ahmed.abhr6@gmail.com>
2023-01-15 22:58:51 -0500

    class9

---
+++ src/class9/AddNumbers.java
@@ -0,0 +1,23 @@
+package class9;
+
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+
+
+
+            sum = sum + i;
+        }
+            System.out.println("sum= " + sum);
+
+
+        }
+
+    }
+
+
+
+
---
+++ src/homewok/blank.java
@@ -0,0 +1,10 @@
+package homewok;
+
+import java.util.Scanner;
+
+public class blank {
+    public static void main(String[] args) {
+        int a = 21 + 67 * 90 % 67;
+int b=34/6*6-34;
+
+    }  }
