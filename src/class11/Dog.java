bf7fa22df2eb17b56c23662300bf502ffdf8a738
tirgani12 <ahmed.abhr6@gmail.com>
2023-01-23 22:03:44 -0500

    homew2

--- src/class11/Dog.java
+++ src/class11/Dog.java
@@ -1,19 +1,19 @@
 package class11;
 
 public class Dog {
-//attributes/properties/fields
-        String name;
-        String breed;
-        int age;
-        double weight;
-        String color;
-        //behaviours
+    //attributes/properties/fields
+    String name;
+    String breed;
+    int age;
+    double weight;
+    String color;
+    //behaviours
     void bark() {
         System.out.println("woof woof");
     }
 
-void sleep(){
-    System.out.println("ZZZZZzzzzzZZZZ");
+    void sleep(){
+        System.out.println("ZZZZZzzzzzZZZZ");
     }
     void eat(){
         System.out.println("dog is eating ...........");
